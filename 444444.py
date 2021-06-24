089fae433a3f885fa6aeb245b3a44612fd8a00cc
ALAMIN <83881970+HAKERBOYMDALAMIN@users.noreply.github.com>
2021-06-24 14:16:46 +0600

    Add files via upload

---
+++ 444444.py
@@ -0,0 +1,96 @@
+import requests, os, time, sys, smtplib
+blue = '\x1b[94m'
+lightblue = '\x1b[94m'
+red = '\x1b[91m'
+white = '\x1b[97m'
+green = '\x1b[93m'
+green = '\x1b[1;32m'
+cyan = '\x1b[96m'
+end = '\x1b[0m'
+yellow = '\n\n\x1b[1;93m'
+
+
+
+
+def alamin(z):
+    for word in z + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.006)
+
+alamin(cyan+"""________________$$$$$\n ______________$$____$$\n ______________$$____$$\n ______________$$____$$\n ______________$$____$$\n ______________$$____$$\n __________$$$$$$____$$$$$$\n ________$$____$$____$$____$$$$\n ________$$____$$____$$____$$__$$\n $$$$$$__$$____$$____$$____$$____$$\n $$____$$$$________________$$____$$\n $$______$$______________________$$\n __$$____$$______________________$$\n ___$$$__$$______________________$$\n ____$$__________________________$$\n _____$$$________________________$$\n ______$$______________________$$$\n _______$$$____________________$$\n ________$$____________________$$\n _________$$$________________$$$\n __________$$________________$$\n __________$$$$$$$$$$$$$$$$$$$$""")
+
+
+def c (z):
+    for word in z + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.006)
+        
+        
+c("peles inter your username and Password ")
+
+
+
+    
+
+print (red+"ENTER YOUR USERNAME & PASSWORD ")	
+
+
+CorrectUsername = "MDALAMIN"
+CorrectPassword = "HAKERBOY"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input(yellow+"Tool Username: ")
+    if (username == CorrectUsername):
+    	password = raw_input(blue+"Tool Password: ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #Dev:MDALAMIN
+	    time.sleep(3)
+            loop = 'false'
+            
+        
+        else:
+            print "Wrong Password"
+            os.system('xdg-open https://www.facebook.com/Mdalmin12345')
+    else:
+        print "Wrong Username"
+        os.system('xdg-open https://www.facebook.com/Mdalmin12345')
+
+
+os.system("clear")
+
+
+
+
+
+
+    
+         
+
+
+
+os.system('xdg-oetapen https://www.facebook.com/l0wk3y.ofc/')
+
+
+
+
+os.system('clear')
+
+def logop(z):
+    for word in z + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.006)
+
+
+
+logop(green+"""                  
+     /\   | |ALAMIN  /\   |\     /| | | | |  | |
+    /  \  | |       /  \  | \   / | | | |  \ | |
+   / /\ \ | |      / /\ \ | |\/ | | | | | | \  |
+  / ____ \| |____ / /__\_\| |   | | | | | |\ \ |
+ /_/    \_\______/_/    \_\_|   |_| | | | | \ \|'""")
+
+
